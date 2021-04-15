a836424132368e711d090dded9ad0fd76f876139
Fazt <contact@faztweb.com>
2021-04-15 03:29:16 -0500

    "Moviendo el Footer a Components y agregando los estilos de Footer"

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './NavBar';
+import Footer from '../components/Footer';
 
 function Layout(props: any) {
     return(
         <React.Fragment>
             <Navbar/>
             {props.children}
+            <Footer />
         </React.Fragment>
     );
 }
