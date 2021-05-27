c440aad55dfbe88bbc330bfd0d9e5cb77a0cacad
joseph <razorij20@gmail.com>
2021-05-27 16:31:51 -0500

    cambios

---
+++ frontend/src/components/Perfil/BotonPerfil.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+type props = {name:string, className:string};
+
+const BotonPerfil: React.FC<props> = (props) => {
+    return(
+        <Link className={props.className} to="/edit">{props.name}</Link>
+    );
+}
+
+export default BotonPerfil;
