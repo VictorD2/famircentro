861853fbd6c72e7ddce6e661b56bbd924b76b75f
JFBJ17 <razorij20@gmail.com>
2021-06-13 01:24:42 -0500

    Alertas agregadas

--- frontend/src/pages/EditPerfil.tsx
+++ frontend/src/pages/EditPerfil.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import swal from 'sweetalert'
 import NavBar from '../components/Helpers/NavBar';
 import Badge from '../components/Helpers/Badge';
 import Footer from '../components/Helpers/Footer';
 import FormEditPerfil from '../components/Perfil/FormEditPerfil';
 
-class EditPerfil extends React.Component {
+const EditPerfil = () => {
 
-    state = {
-        profileImg: "https://picsum.photos/200/300"
-    }
+    const [profileImg, setProfileImg] = useState<string | ArrayBuffer | null>("https://picsum.photos/200/300");
 
-    handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
         e.preventDefault();
         e.stopPropagation();
     }
 
-    handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
         const tipos = ['image/gif', 'image/png', 'image/jpeg', 'image/bmp', 'image/webp']
         e.preventDefault();
         if (e.dataTransfer.files instanceof FileList) {
             if (tipos.includes(e.dataTransfer.files[0].type)) {
                 const reader = new FileReader();
                 reader.onload = () => {
-                    if (reader.readyState === 2) this.setState({ profileImg: reader.result });
+                    if (reader.readyState === 2) setProfileImg(reader.result);
                 }
                 reader.readAsDataURL(e.dataTransfer.files[0]);
             } else {
@@ -42,14 +40,14 @@ class EditPerfil extends React.Component {
         }
     }
 
-    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const tipos = ['image/gif', 'image/png', 'image/jpeg', 'image/bmp', 'image/webp']
         if (e.target.files instanceof FileList) {
             if (tipos.includes(e.target.files[0].type)) {
                 const reader = new FileReader();
                 reader.onload = () => {
                     if (reader.readyState === 2) {
-                        this.setState({ profileImg: reader.result });
+                        setProfileImg(reader.result);
                     }
                 }
                 reader.readAsDataURL(e.target.files[0]);
@@ -69,39 +67,37 @@ class EditPerfil extends React.Component {
         }
     }
 
-    render() {
-        return (
-            <React.Fragment>
-                <NavBar />
+    return (
+        <React.Fragment>
+            <NavBar />
 
-                <Badge name="Editar Perfil" />
+            <Badge name="Editar Perfil" />
 
-                <div className="Main__container">
-                    <div className="container bg-light mt-5" style={{ marginBottom: "4.5rem" }}>
-                        <div className="row p-5">
-                            <div className="col-6">
-                                <div draggable="true" className="cuadroEditPerfil" onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
-                                    <figure className="editProfile-img">
-                                        <img id="avatar" src={this.state.profileImg} alt="Mi avatar" width="200" height="200" />
-                                    </figure>
-                                    <div className="d-grid gap-2 col-6 mx-auto">
-                                        <input type="file" id="inputFile" style={{ display: "none" }} onChange={this.handleChange} />
-                                        <button className="btn btn-editPerfil" type="button">
-                                            <label htmlFor="inputFile" className="d-block" style={{ cursor: "pointer" }}>Subir foto de perfil</label>
-                                        </button>
-                                    </div>
+            <div className="Main__container">
+                <div className="container bg-light mt-5" style={{ marginBottom: "4.5rem" }}>
+                    <div className="row p-5">
+                        <div className="col-6">
+                            <div draggable="true" className="cuadroEditPerfil" onDragOver={handleDragOver} onDrop={handleDrop}>
+                                <figure className="editProfile-img">
+                                    <img id="avatar" src={profileImg} alt="Mi avatar" width="200" height="200" />
+                                </figure>
+                                <div className="d-grid gap-2 col-6 mx-auto">
+                                    <input type="file" id="inputFile" style={{ display: "none" }} onChange={handleChange} />
+                                    <button className="btn btn-editPerfil" type="button">
+                                        <label htmlFor="inputFile" className="d-block" style={{ cursor: "pointer" }}>Subir foto de perfil</label>
+                                    </button>
                                 </div>
                             </div>
-                            <div className="col-6">
-                                <FormEditPerfil />
-                            </div>
+                        </div>
+                        <div className="col-6">
+                            <FormEditPerfil />
                         </div>
                     </div>
                 </div>
-                <Footer />
-            </React.Fragment>
-        );
-    }
+            </div>
+            <Footer />
+        </React.Fragment>
+    );
 }
 
 export default EditPerfil;
